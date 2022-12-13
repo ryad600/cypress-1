dd517f87bccecbeb99d483a1f8fa882c70648ed7
Stephan Hug <stephan.hug@adfinis.com>
2022-12-13 10:00:44 +0100

    commit the config file

---
+++ webapp/API/V1/public/util/config.php
@@ -0,0 +1,11 @@
+<?php
+	//API credentials.
+	$api_username = "root";
+	$api_password = "sUP3R53CR3T#";
+
+	//Database connection information.
+	$db_hostname = "mariadb";
+	$db_username = "root";
+	$db_password = "root";
+	$db_database = "shop";
+?>
