87178808c0394f6e3f90ba3ddca65ebdb6529836
Stephan Hug <stephan.hug@adfinis.com>
2022-12-18 10:56:24 +0100

    feat: basic unit test setup

---
+++ cypress/cypress/e2e/unit-tests.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="Cypress" />
+
+describe("validateUsername function", () => {
+  before(() => {
+    cy.visit('localhost')
+  })
+
+  it('write your tests here', () => {
+    cy.window().then((window) => {
+
+    })
+  })
+})
